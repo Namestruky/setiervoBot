82b0f09d03cbcbfac8fdba47aaebcb0f9b7b53e7
Salva <salva3_77@hotmail.com>
2023-04-05 23:48:51 +0200

    nuevos sonidos y cambios en el reproductor

--- commands/playsounds.js
+++ commands/playsounds.js
@@ -2,6 +2,8 @@ const { randomRange } = require('../utils.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { createAudioPlayer, createAudioResource, joinVoiceChannel, AudioPlayerStatus } = require('@discordjs/voice');
 const play = require('./play.js');
+const fs = require('fs');
+
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -16,33 +18,46 @@ module.exports = {
             const voiceChannel = client.channels.cache.get(voice.channelId);
             const guildId= '196260070261129216';
             
-            const player = createAudioPlayer();
+
+            if(parameter == 'listar'){
+               
+                const files = await fs.promises.readdir('sounds');
+                const listado = [];
+
+                files.forEach(element => listado.push(" "+element.substr(0,element.indexOf('.'))));
+
+                message.reply("Litado de la lista de sonidos --> " + listado);
+
+            }else{
+                const player = createAudioPlayer();
             
-            try {
-                const resource = createAudioResource(`sounds/${parameter}.mp3`);
+                try {
+                    const resource = createAudioResource(`sounds/${parameter}.mp3`);
 
-                player.on('error', (error) => {
-                    console.log("No existe ese audio");
-                });
+                    player.on('error', (error) => {
+                        console.log("No existe ese audio");
+                    });
 
-                player.play(resource);
+                    player.play(resource);
 
-                const conexionCanal = joinVoiceChannel({
-                    channelId: voiceChannelId,
-                    guildId: guildId,
-                    adapterCreator: voiceChannel.guild.voiceAdapterCreator,
-                });
-    
-                const subscripcion = conexionCanal.subscribe(player);
-    
-                //Cuando termina lo que este reproduciendo, se sale
-                player.on(AudioPlayerStatus.Idle, () => {
-                    subscripcion.unsubscribe();
-                    conexionCanal.disconnect();
-                });
-            } catch (error) {
-                console.log(error);
-            } 
+                    const conexionCanal = joinVoiceChannel({
+                        channelId: voiceChannelId,
+                        guildId: guildId,
+                        adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+                    });
+        
+                    const subscripcion = conexionCanal.subscribe(player);
+        
+                    //Cuando termina lo que este reproduciendo, se sale
+                    player.on(AudioPlayerStatus.Idle, () => {
+                        subscripcion.unsubscribe();
+                        conexionCanal.disconnect();
+                    });
+                } catch (error) {
+                    console.log(error);
+                } 
+            }
+            
         }
 	}
 };
